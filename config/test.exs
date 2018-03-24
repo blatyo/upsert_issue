9e60594e60fc0e205d5ec4bcf95cbac91e4afc57
Allen Madsen <blatyo@gmail.com>
2018-03-24 19:28:34 -0400

    Change log level to see SQL

--- config/test.exs
+++ config/test.exs
@@ -7,7 +7,7 @@ config :upsert_issue, UpsertIssueWeb.Endpoint,
   server: false
 
 # Print only warnings and errors during test
-config :logger, level: :warn
+config :logger, level: :debug
 
 # Configure your database
 config :upsert_issue, UpsertIssue.Repo,
